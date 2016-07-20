478df04b25bd54521e6f00de58d5f62e39ddb3bf
zombiQWERTY <zombiqwerty@yandex.ru>
2016-07-20 16:51:37 +0300

    Cors configuired

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -5,10 +5,49 @@ import cors       from 'koa-cors';
 import bodyParser from 'koa-bodyparser';
 import session    from 'koa-generic-session';
 
+const corsConfig = () => {
+  const accessControlMaxAge = '1200';
+
+  const allowedOrigins = [
+    'http://localhost:8080'
+  ];
+
+  const accessControlAllowMethods = [
+    'OPTIONS',
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'HEAD'
+  ];
+
+  const accessControlAllowHeaders = [
+    'X-Requested-With',
+    'If-Modified-Since',
+    'Cache-Control',
+    'DNT',
+    'X-CustomHeader',
+    'Keep-Alive',
+    'User-Agent',
+    'Content-Type',
+    'Authorization',
+    'Pragma'
+  ];
+
+  return {
+    origin:      allowedOrigins,
+    methods:     accessControlAllowMethods,
+    headers:     accessControlAllowHeaders,
+    expose:      'Authorization',
+    maxAge:      accessControlMaxAge,
+    credentials: true
+  };
+};
+
 export default function middleware() {
   return compose([
     logger(),
-    convert(cors()),
+    convert(cors(corsConfig())),
     convert(bodyParser()),
     convert(session())
   ]);
